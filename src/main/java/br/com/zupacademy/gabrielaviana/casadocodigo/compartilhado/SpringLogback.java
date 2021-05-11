596faf83e9e8b752ebcc756e82303ab13196be36
eugabiviana <gabi.viana88@gmail.com>
2021-05-11 09:44:08 -0300

    Implementação do Actuator

---
+++ src/main/java/br/com/zupacademy/gabrielaviana/casadocodigo/compartilhado/SpringLogback.java
@@ -0,0 +1,17 @@
+package br.com.zupacademy.gabrielaviana.casadocodigo.compartilhado;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpringLogback {
+
+	private final Logger logger = LoggerFactory.getLogger(SpringLogback.class);
+
+    public void log() {
+        logger.info("Log de informação");
+        logger.warn("Log de aviso, algo está errado ou faltando! cuidado!");
+        logger.error("Log de erro, algo de errado aconteceu!");
+        logger.debug("Log de depuração, contém informações mais refinadas, que são mais úteis para depurar um aplicativo");
+        logger.trace("Log de rastreabilidade, contém informações mais refinadas do que o DEBUG");
+    }
+}
